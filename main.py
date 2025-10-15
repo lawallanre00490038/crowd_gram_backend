4786e29459aaca671a38a6276db97d834214e9d2
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-10-15 05:17:24 +0100

    added auto revewer2

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app", 
-        host="127.0.0.1",
-        port=8000,
+        host="0.0.0.0",
+        port=10000,
         reload=True
     )
